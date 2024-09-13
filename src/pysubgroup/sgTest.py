750b145bd9f60794e8de14c601afc7ee00e340e0
AlexanderPuff <ale.puff@icloud.com>
2024-09-13 10:47:54 +0200

    Testing synth data

--- src/pysubgroup/sgTest.py
+++ src/pysubgroup/sgTest.py
@@ -1,34 +1,64 @@
+from io import StringIO
+import pandas as pd
+import pkg_resources
 import pysubgroup as ps
 import numpy as np
 from cupyx.profiler import benchmark as bm
 import cudf
-import time
-
-# Load the example dataset
-from pysubgroup.datasets import get_titanic_data, get_titanic_gpu
-data = get_titanic_data()
-dataGPU = get_titanic_gpu()
+import datetime
 
 
-target = ps.BinaryTarget ('Survived', True)
-searchspace = ps.create_selectors(data, ignore=['Survived'])
-task = ps.SubgroupDiscoveryTask (
-    data,
-    target,
-    searchspace,
-    result_set_size=10,
-    depth=3,
-    qf=ps.WRAccQF())
-result=ps.DFS(ps.BitSetRepresentation).execute(task)
 
-print(result.to_dataframe())
-if False:
-    gputask = ps.SubgroupDiscoveryTask (
-        dataGPU,
+def get_data_gpu(filepath):
+    s_io = StringIO(
+        str(pkg_resources.resource_string("pysubgroup", filepath), "utf-8")
+    )
+    return pd.read_csv(s_io, sep="\t", header=0, nrows=100000)
+
+synth="data/synthetic/synth_titanic.csv"
+normal="data/titanic.csv"
+
+data=get_data_gpu(normal)
+
+if(True):
+    target= ps.BinaryTarget('Survived', True)
+    searchspace=ps.create_selectors(data, ignore=['Survived'])
+    task = ps.SubgroupDiscoveryTask (
+            data,
+            target,
+            searchspace,
+            result_set_size=10,
+            depth=2,
+            qf=ps.WRAccQF())
+    a = datetime.datetime.now()
+    result=ps.DFS(ps.BitSetRepresentation).execute(task)
+    b = datetime.datetime.now()
+    print(b-a)
+    result=ps.DFS(ps.CUDABitSetRepr).execute(task)
+    c = datetime.datetime.now()
+    print(c-b)
+
+
+if(False):
+    target = ps.BinaryTarget ('Survived', True)
+    searchspace = ps.create_selectors(data, ignore=['Survived'])
+    task = ps.SubgroupDiscoveryTask (
+        data,
         target,
         searchspace,
-        result_set_size=5,
-        depth=2,
+        result_set_size=10,
+        depth=3,
         qf=ps.WRAccQF())
-    print(bm(ps.DFS(ps.CUDABitSetRepr).execute, (gputask,), n_repeat=5))
-    print(bm(ps.DFS().execute, (task,), n_repeat=5))
+    result=ps.DFS(ps.BitSetRepresentation).execute(task)
+
+    print(result.to_dataframe())
+    if False:
+        gputask = ps.SubgroupDiscoveryTask (
+            dataGPU,
+            target,
+            searchspace,
+            result_set_size=5,
+            depth=2,
+            qf=ps.WRAccQF())
+        print(bm(ps.DFS(ps.CUDABitSetRepr).execute, (gputask,), n_repeat=5))
+        print(bm(ps.DFS().execute, (task,), n_repeat=5))
--- src/pysubgroup/synthetic_data.py
+++ src/pysubgroup/synthetic_data.py
@@ -1,5 +1,23 @@
-from sdv.metadata import SingleTableMetadata
+import sdv.datasets.demo as sdd
+from sdv.single_table import GaussianCopulaSynthesizer
+import pandas as pd
 
-d = SingleTableMetadata()
-d.detect_from_csv(filepath='/home/alexpuff/datasets/titanic.csv')
-print(d.to_dict())
+def load_titanic():
+    from pysubgroup.datasets import get_titanic_data
+    data = get_titanic_data()
+    from sdv.metadata import SingleTableMetadata as stm
+    metadata = stm()
+    metadata.detect_from_dataframe(data)
+    return data, metadata
+
+def make_sampler(data, metadata):
+    synth=GaussianCopulaSynthesizer(metadata)
+    synth.fit(data)
+    return synth
+
+
+synth=GaussianCopulaSynthesizer.load(filepath='pysubgroup/src/pysubgroup/data/synthetic/titanic.pkl')
+synth_data=synth.sample(num_rows=1000000)
+synth_data.to_csv('pysubgroup/src/pysubgroup/data/synthetic/synth_titanic.csv', index=False)
+
+
